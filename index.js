770c6263bb7a371f5970d0d229a1791547d68356
Sharon Rolel <sharonrolel@gmail.com>
2016-05-22 22:06:51 +0300

    adding build folder with transpiled files

--- index.js
+++ index.js
@@ -1,11 +1,1 @@
-import Plugins from './src/plugins.js';
-import Loader from './src/loader.js';
-
-const { fetch, bundle } = new Loader([
-  Plugins.values,
-  Plugins.localByDefault,
-  Plugins.extractImports,
-  Plugins.scope
-]);
-
-export { Loader, Plugins, fetch, bundle };
+module.exports = require('./build/index.js');
